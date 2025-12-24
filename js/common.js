c3fdd78dca968c809e46cfabf28a354f2e9766fb
eastory <eastory@.com>
2025-12-24 15:30:44 +0800

    抽取轻提示函数

--- js/common.js
+++ js/common.js
@@ -1,1 +1,13 @@
-axios.defaults.baseURL = 'https://hmajax.itheima.net'
+axios.defaults.baseURL = "https://hmajax.itheima.net";
+
+// 抽取轻提示函数
+function showToast(msg) {
+  const toastDom = document.querySelector(".my-toast");
+  // 实例化toast组件
+  const toast = new bootstrap.Toast(toastDom);
+  // 修改内容并显示
+  document.querySelector(".toast-body").innerText = msg;
+  toast.show();
+}
+// showToast("hahahahaha");
+
