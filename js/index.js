c590a62aa9a6f0b7f2894941b1db7f3ebb60127a
eastory <eastory@.com>
2025-12-24 17:23:14 +0800

    渲染统计数据

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@ checkLogin()
 renderUsername()
 
 registerLogout()
+
+async function getData() {
+  const res = await axios({
+    url:'/dashboard',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    }
+  })
+  console.log(res);
+  
+  const overview = res.data.data.overview
+
+  Object.keys(overview).forEach(key => {
+    document.querySelector(`.${key}`).innerText = overview[key]
+  });
+}
+getData()
