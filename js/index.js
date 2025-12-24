2ee2f8828315975ced70210bc4cba928820fb0d2
eastory <eastory@.com>
2025-12-24 17:33:06 +0800

    登录状态失效

--- js/index.js
+++ js/index.js
@@ -5,18 +5,28 @@ renderUsername()
 registerLogout()
 
 async function getData() {
-  const res = await axios({
-    url:'/dashboard',
-    headers: {
-      Authorization: localStorage.getItem('token')
-    }
-  })
-  console.log(res);
-  
-  const overview = res.data.data.overview
+  try {
+    const res = await axios({
+      url:'/dashboard',
+      headers: {
+        Authorization: localStorage.getItem('token')
+      }
+    })
+    
+    const overview = res.data.data.overview
+    Object.keys(overview).forEach(key => {
+      document.querySelector(`.${key}`).innerText = overview[key]
+    });
 
-  Object.keys(overview).forEach(key => {
-    document.querySelector(`.${key}`).innerText = overview[key]
-  });
+  }catch(error) {
+    if (error.response.status === 401) {
+      showToast('登录过期，请重新登录')
+      localStorage.removeItem('token')
+      localStorage.removeItem('username')
+      setTimeout(() => {
+        location.href = './login.html'
+      }, 1500);
+    }
+  }
 }
 getData()
