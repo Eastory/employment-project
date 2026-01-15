ab91b35e40e8198306b51c64b13ec19758b813ac
eastory <eastory@.com>
2026-01-15 22:44:19 +0800

    完成数据看板-薪资走势

--- js/index.js
+++ js/index.js
@@ -10,6 +10,88 @@ function renderOverview(overview) {
     document.querySelector(`.${key}`).innerText = overview[key]
   });
 }
+// 渲染薪资走势
+function renderYearSalary(year) {
+  const myChart = echarts.init(document.querySelector('#line'))
+
+  const option = {
+    title: {
+      text: '2022薪资走势',
+      top: 10,
+      left: 10
+    },
+    tooltip: {
+      show: 'true',
+      trigger: 'axis'
+    },
+    grid: {
+      top: 80
+    },
+    xAxis: {
+      data: year.map(ele => ele.month),
+      axisLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#bbb'
+        }
+      }
+    },
+    yAxis: {
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
+    },
+    series: [
+      {
+        name: '薪资',
+        data: year.map(ele => ele.salary),
+        type: 'line',
+        smooth: true,
+        itemStyle: {
+          color: '#668FE9'
+        },
+        lineStyle: {
+          width: 6,
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 1,
+            y2: 0,
+            colorStops: [{
+                offset: 0, color: '#5CA0E7' // 0% 处的颜色
+            }, {
+                offset: 1, color: '#657EEB' // 100% 处的颜色
+            }],
+            global: false // 缺省为 false
+          }
+        },
+        areaStyle: {
+          origin: 'auto',
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [{
+                offset: 0, color: '#89B9F1' // 0% 处的颜色
+            }, {
+                offset: 1, color: 'rgba(255, 255, 255, 0)' // 100% 处的颜色
+            }],
+            global: false // 缺省为 false
+          }
+        },
+        symbolSize: 10
+      }
+    ]
+  };
+
+  myChart.setOption(option)
+
+}
 
 // 获取统计数据
 async function getData() {
@@ -19,8 +101,9 @@ async function getData() {
     })
     console.log(res);
 
-    const {overview} = res.data
+    const {overview, year} = res.data
 
     renderOverview(overview)
+    renderYearSalary(year)
 }
 getData()
