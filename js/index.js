0852114503c6defe1fa2e6567c93cb821ac98330
eastory <eastory@.com>
2026-01-16 13:48:15 +0800

    完成数据看板-班级每组薪资

--- js/index.js
+++ js/index.js
@@ -142,6 +142,88 @@ function renderClassSalary(salaryData) {
 
   myChart.setOption(option)
 }
+// 渲染每组薪资
+function renderGroupSalary(groupData, groupNumber = 1) {
+  
+  const myChart = echarts.init(document.querySelector('#lines'))
+
+  const option = {
+    grid: {
+      left:78,
+      top: 30,
+      right: 30,
+      bottom: 50
+    },
+    xAxis: {
+      type: 'category',
+      data: groupData[1].map(ele => ele.name),
+      axisLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#bbb'
+        }
+      },
+      // 横轴标签相关设置
+      axisLabel: {
+        // 横轴文字颜色，默认和 lineStyle 颜色一样
+        color: '#888'
+      }
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
+    },
+    tooltip:{},
+    series: [
+      {
+        name: '期望薪资',
+        data: groupData[groupNumber].map(ele => ele.hope_salary),
+        type: 'bar',
+        itemStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [{
+                offset: 0, color: '#34D39A' // 0% 处的颜色
+            }, {
+                offset: 1, color: 'rgba(52,211,154,0.2)' // 100% 处的颜色
+            }],
+            global: false // 缺省为 false
+          }
+        }
+      },
+      {
+        name: '实际薪资',
+        data: groupData[groupNumber].map(ele => ele.salary),
+        type: 'bar',
+        itemStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [{
+                offset: 0, color: '#499FEE' // 0% 处的颜色
+            }, {
+                offset: 1, color: 'rgba(73,159,238,0.2)' // 100% 处的颜色
+            }],
+            global: false // 缺省为 false
+          }
+        }
+      }
+    ]
+  };
+
+  myChart.setOption(option)
+}
 
 // 获取统计数据
 async function getData() {
@@ -151,10 +233,20 @@ async function getData() {
     })
     console.log(res);
 
-    const {overview, year, salaryData} = res.data
+    const {overview, year, salaryData, groupData} = res.data
 
     renderOverview(overview)
     renderYearSalary(year)
     renderClassSalary(salaryData)
+    renderGroupSalary(groupData)
+
+    document.querySelector('.card-header').addEventListener('click', function(e) {
+      if (e.target.tagName !== 'BUTTON' || e.target.classList.contains('btn-blue')) {
+        return
+      }
+      document.querySelector('.btn-blue').classList.remove('btn-blue')
+      e.target.classList.add('btn-blue')
+      renderGroupSalary(groupData, e.target.innerText)
+    })
 }
 getData()
